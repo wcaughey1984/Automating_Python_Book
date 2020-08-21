1eaf9c2dd8239af11d539a7f53458d8a6ff31a6c
wcaughey-tt <wcaughey@thumbtack.com>
2020-08-21 10:38:20 -0600

    more jupyter notebooks

---
+++ Chapter 3 Functions.py
@@ -0,0 +1,19 @@
+"""
+
+    Title:      Chapter 3 Functions
+    Purpose:    (Knowledge Development) Chapter 3 code and notes on functions
+    Author:     Billy Caughey
+    Date:       2020.08.18 - Initial Build
+
+"""
+
+# A function is a mini-program within a program
+
+
+def hello():
+    print("Howdy!")
+    print("Howdy!!!")
+    print("Hello there.")
+
+
+hello()
